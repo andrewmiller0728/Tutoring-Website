8a0b1005645d9d2a54f8ec465423e8a5d9337b82
andrewmiller0728 <andrewmiller0728@gmail.com>
2021-02-28 23:10:30 -0500

    php form testing

---
+++ formtest.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <p>Testing the functionality of HTML forms and PHP</p>
+        <?php
+            echo "Name " . $_GET['name'] . " => " . $_POST['rating'];
+        ?>
+    </body>
+</html>
--- sqltest.php
+++ sqltest.php
@@ -1,4 +1,5 @@
 <?php
+    // set up sql server connection
     $serverName = "andrewstutoringservices.database.windows.net";
     $connectionOptions = array(
         "Database" => "tutoring-data",
@@ -10,6 +11,8 @@
          FROM [SalesLT].[ProductCategory] pc
          JOIN [SalesLT].[Product] p
          ON pc.productcategoryid = p.productcategoryid";
+
+    // query database
     $getResults= sqlsrv_query($conn, $tsql);
     echo ("Reading data from table" . PHP_EOL);
     if ($getResults == FALSE)
@@ -17,5 +20,7 @@
     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
      echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
     }
+
+    // free memory
     sqlsrv_free_stmt($getResults);
 ?>
